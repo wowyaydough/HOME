1d2da9d0ec0661083f12d19625c951b267858f44
Hwang DoYeon <150096210+wowyaydough@users.noreply.github.com>
2024-12-02 10:05:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,26 @@
-// mode change (dark <-> light)
-let modeChangeBtn = document.querySelector('.gnb-mode-change-btn')
-
-modeChangeBtn.addEventListener('click', function () {
-  const containDark = modeChangeBtn.classList.contains('to-dark')
-
-  if (containDark === false) {
-    modeChangeBtn.classList.add('to-dark')
-    modeChangeBtn.innerHTML = 'Light'
-  } else {
-    modeChangeBtn.classList.remove('to-dark')
-    modeChangeBtn.innerHTML = 'Dark'
-  }
-})
-
-// slide
-
+// slide variables
 let pagingNumber = 1
 let portfolioWidth = 0
 let tabNumber = 0
 
-// tab-page
-function tabHide() {
-  const tabPage = document.querySelectorAll('.tab-page')
-  const tabControl = (tabNumber) => {
-    if (tabPage[tabNumber].classList.contains('hide') === true) {
-      tabPage[tabNumber].classList.remove('hide')
-    } else {
-      tabPage[tabNumber].classList.add('hide')
-    }
+// responsive
+const responsiveSize = () => {
+  if (window.outerWidth < 768) {
+    portfolioWidth = 50
+  } else if (window.outerWidth > 1199) {
+    portfolioWidth = 30
+  } else {
+    portfolioWidth = 40
   }
 }
 
-const tabBtn = document.querySelectorAll('.tab-btn')
-tabBtn[0].addEventListener('click', function () {
-  tabNumber = 0
-  tabHide(0)
-  console.log('0')
-})
-tabBtn[1].addEventListener('click', function () {
-  tabNumber = 1
-  tabHide(1)
-  console.log('2')
-})
+const responsiveAdjust = () => {
+  let startWidth = portfolioWidth / 2 + portfolioWidth * 2
+  let calWidth = startWidth - (pagingNumber - 1) * portfolioWidth
+  document.querySelector(
+    '.portfolio-list'
+  ).style.transform = `translate(${calWidth}vw, 0)`
+}
 
 // 슬라이드
 const slideFn = () => {
@@ -67,22 +46,12 @@ const slideFn = () => {
       }
     }
 
-    // 반응형 슬라이드 크기
-    if (window.outerWidth < 768) {
-      portfolioWidth = 50
-    } else if (window.outerWidth > 1199) {
-      portfolioWidth = 30
-    } else {
-      portfolioWidth = 40
-    }
+    responsiveSize()
 
     // pagination
     pagingNumber = Number(targetTxT)
-    let startWidth = portfolioWidth / 2 + portfolioWidth * 2
-    let calWidth = startWidth - (pagingNumber - 1) * portfolioWidth
-    document.querySelector(
-      '.portfolio-list'
-    ).style.transform = `translate(${calWidth}vw, 0)`
+
+    responsiveAdjust()
   })
 }
 
@@ -92,19 +61,10 @@ slideFn()
 
 // window resize, slide start
 const resize = () => {
-  document.querySelector(window).addEventListener('change', function () {
-    if (window.outerWidth < 768) {
-      portfolioWidth = 50
-    } else if (window.outerWidth > 1199) {
-      portfolioWidth = 30
-    } else {
-      portfolioWidth = 40
-    }
-    let startWidth = portfolioWidth / 2 + portfolioWidth * 2
-    document.querySelector(
-      '.portfolio-list'
-    ).style.transform = `translate(${startWidth}vw, 0)`
-  })
+  responsiveSize()
+
+  pagingNumber = 1
+  responsiveAdjust()
 }
 
 //  click to open github
